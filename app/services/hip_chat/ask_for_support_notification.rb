df5bd200308c92b1ab7bff774260ae3a94b0e36d
arturro <artchru@gmail.com>
2014-05-05 13:42:43 +0200

    unused files

--- app/services/hip_chat/ask_for_support_notification.rb
+++
@@ -1,9 +0,0 @@
-module HipChat
-  class AskForSupportNotification < HipChat::Notification
-   
-    def message
-      "#{model.receiver} asked for support #{model.user} - #{url}"  
-    end
-    
-  end
-end
--- app/services/hip_chat/finish_support_notification.rb
+++
@@ -1,9 +0,0 @@
-module HipChat
-  class FinishSupportNotification < HipChat::Notification
-
-    def message
-      "#{model.user} helped #{model.receiver} with #{model.topic} - #{url}"
-    end
-
-  end
-end
