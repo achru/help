d2f9907f71c8cc64f7ae40d9ec5f1589f604646d
arturro <artchru@gmail.com>
2014-05-05 13:42:20 +0200

    override url

--- app/services/hip_chat/comment_on_support_notification.rb
+++ app/services/hip_chat/comment_on_support_notification.rb
@@ -1,10 +1,6 @@
 module HipChat
   class CommentOnSupportNotification < HipChat::Notification
 
-    def message
-      "#{model.user} commented on support - #{url}"
-    end
-
     def url
       url_for(model.support)
     end
